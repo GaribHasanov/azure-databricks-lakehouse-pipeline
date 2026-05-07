01bbd8c57717fb772beb9580b543e864848ee8c6
GaribHasanov <60735401+GaribHasanov@users.noreply.github.com>
2026-05-07 23:57:12 +0200

    Update main.tf

--- ci-cd/terraform/main.tf
+++ ci-cd/terraform/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -9,43 +11,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-}
-
-# ---------------------------
-# Resource Group
-# ---------------------------
-resource "azurerm_resource_group" "rg" {
-  name     = "lakehouse-rg"
-  location = "West Europe"
-}
-
-# ---------------------------
-# Storage Account (ADLS Gen2)
-# ---------------------------
-resource "azurerm_storage_account" "adls" {
-  name                     = "lakehousestorageacct"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  is_hns_enabled           = true
-}
-
-# ---------------------------
-# Data Factory
-# ---------------------------
-resource "azurerm_data_factory" "adf" {
-  name                = "lakehouse-adf"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-# ---------------------------
-# Databricks Workspace
-# ---------------------------
-resource "azurerm_databricks_workspace" "dbw" {
-  name                = "lakehouse-databricks"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "standard"
 }
