f9e5f97c51acfea567b2508cafea8db5af259360
GaribHasanov <60735401+GaribHasanov@users.noreply.github.com>
2026-05-07 23:38:22 +0200

    Create constants.py

---
+++ src/utils/constants.py
@@ -0,0 +1,6 @@
+class Constants:
+    SALES_PATH = "sales_transactions"
+    PRODUCTS_PATH = "products"
+    STORES_PATH = "stores"
+    CUSTOMERS_PATH = "customers"
+    PROMOTIONS_PATH = "promotions"
