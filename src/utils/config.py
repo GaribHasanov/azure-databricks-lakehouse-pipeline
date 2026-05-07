4c3741427e878601b1338a63d66295260f2aa611
GaribHasanov <60735401+GaribHasanov@users.noreply.github.com>
2026-05-07 23:34:37 +0200

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,35 +3,17 @@ from dataclasses import dataclass
 
 
 @dataclass
-class StorageConfig:
+class Config:
     raw_path: str
     bronze_path: str
     silver_path: str
     gold_path: str
 
 
-@dataclass
-class AppConfig:
-    storage: StorageConfig
-    eventhub_connection_string: str
-
-
-def load_config() -> AppConfig:
-    """
-    Central application configuration loader
-    """
-
-    storage = StorageConfig(
-        raw_path=os.getenv("RAW_PATH", "/mnt/data/raw/"),
-        bronze_path=os.getenv("BRONZE_PATH", "/mnt/data/bronze/"),
-        silver_path=os.getenv("SILVER_PATH", "/mnt/data/silver/"),
-        gold_path=os.getenv("GOLD_PATH", "/mnt/data/gold/")
-    )
-
-    return AppConfig(
-        storage=storage,
-        eventhub_connection_string=os.getenv(
-            "EVENTHUB_CONNECTION_STRING",
-            ""
-        )
+def get_config() -> Config:
+    return Config(
+        raw_path=os.getenv("RAW_PATH", "/data/raw/"),
+        bronze_path=os.getenv("BRONZE_PATH", "/data/bronze/"),
+        silver_path=os.getenv("SILVER_PATH", "/data/silver/"),
+        gold_path=os.getenv("GOLD_PATH", "/data/gold/")
     )
