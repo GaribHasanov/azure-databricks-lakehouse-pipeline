6176da5cad4120d0b7619ff3f757d35125de5671
GaribHasanov <60735401+GaribHasanov@users.noreply.github.com>
2026-05-07 23:59:06 +0200

    Add Azure Key Vault resource configuration

---
+++ ci-cd/terraform/keyvault.tf
@@ -0,0 +1,7 @@
+resource "azurerm_key_vault" "kv" {
+  name                = "retail-kv"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+}
